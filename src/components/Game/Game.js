f7f425b7808d6aec1833d0c65cd29e645cf96adf
Ondrej Mockor <78723474+lenondrej@users.noreply.github.com>
2024-08-08 10:32:36 +0200

    refactor and polish

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import  GuessInput  from '../GuessInput/GuessInput'
+import React from 'react'
+import GuessInput from '../GuessInput/GuessInput'
+import HappyBanner from '../HappyBanner/HappyBanner'
+import SadBanner from '../SadBanner/SadBanner'
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-import GuessResults from '../GuessResults/GuessResults';
+import { sample } from '../../utils'
+import { WORDS } from '../../data'
+import GuessResults from '../GuessResults/GuessResults'
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({ answer })
 
 function Game() {
-  const [guesses, setGuesses] = React.useState([])
-  const [status, setStatus] = React.useState('running')
+	const [guesses, setGuesses] = React.useState([])
+	const [status, setStatus] = React.useState('running')
+	const numOfGuesses = guesses.length
 
-  const numOfGuesses = guesses.length
+	function getGameStatus(submission) {
+		if (submission === answer) {
+			setStatus('won')
+		} else if (
+			(submission !== answer) &
+			(guesses.length >= NUM_OF_GUESSES_ALLOWED - 1)
+		) {
+			setStatus('lost')
+		}
+	}
 
-  function getGameStatus(submission){
-    if (submission === answer){
-      setStatus('won')
-    } else if (submission !== answer & guesses.length === 5){
-      setStatus('lost')
-    }
-  }
-  
-  function handleGuessSubmit(newSubmission){
-    const newGuess = {answer: newSubmission, id: crypto.randomUUID()}
-    const updatedGuessList = [...guesses, newGuess]
-    setGuesses(updatedGuessList)
-    getGameStatus(newGuess.answer)
-  }
+	function handleGuessSubmit(newSubmission) {
+		const newGuess = { answer: newSubmission, id: crypto.randomUUID() }
+		const updatedGuessList = [...guesses, newGuess]
+		setGuesses(updatedGuessList)
+		getGameStatus(newGuess.answer)
+	}
 
-  return <>
-    <GuessResults guesses={guesses} answer={answer}/>
-    <GuessInput handleGuessSubmit={handleGuessSubmit} status={status} answer={answer} numOfGuesses={numOfGuesses}/>
-  </>;
+	return (
+		<>
+			<GuessResults guesses={guesses} answer={answer} />
+			{status === 'won' ? (
+				<HappyBanner numOfGuesses={numOfGuesses} />
+			) : undefined}
+			{status === 'lost' ? <SadBanner answer={answer} /> : undefined}
+
+			<GuessInput handleGuessSubmit={handleGuessSubmit} status={status} />
+		</>
+	)
 }
 
-export default Game;
+export default Game
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,46 +1,36 @@
-import React from 'react';
+import React from 'react'
 
-function GuessInput({handleGuessSubmit, status, answer, numOfGuesses}) {
-  const [word, setWord] = React.useState('')
+function GuessInput({ handleGuessSubmit, status }) {
+	const [word, setWord] = React.useState('')
 
-  function handleWordSubmit(event){
-    event.preventDefault()
-    handleGuessSubmit(word)
-    setWord('')
-  }
+	function handleWordSubmit(event) {
+		event.preventDefault()
+		handleGuessSubmit(word)
+		setWord('')
+	}
 
-    const happyBanner = <div className="happy banner">
-    <p>
-      <strong>Congratulations!</strong> Got it in
-      <strong>{numOfGuesses} guesses</strong>.
-    </p>
-  </div>
+	return (
+		<div>
+			<form
+				className='guess-input-wrapper'
+				onSubmit={event => handleWordSubmit(event)}
+			>
+				<label htmlFor='guess-input'>Enter guess:</label>
 
-  const sadBanner =  (<div className="sad banner">
-  <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
-  </div>)
-
-
-  return <div>
-    <form className="guess-input-wrapper" onSubmit={(event) => handleWordSubmit(event)}>
-      <label htmlFor="guess-input">Enter guess:</label>
-
-      { status === 'won' ? happyBanner : undefined}
-      { status === 'lost' ? sadBanner : undefined}
-
-      <input 
-        id="guess-input" 
-        type="text" 
-        maxLength={5}
-        pattern='[A-Za-z]{5}'
-        title="5 letter word"
-        required={true} 
-        value={word} 
-        onChange={(e)=> setWord(e.target.value.toUpperCase())}
-        disabled={status !== 'running'}
-      />
-    </form>
-  </div>;
+				<input
+					id='guess-input'
+					type='text'
+					maxLength={5}
+					pattern='[A-Za-z]{5}'
+					title='5 letter word'
+					required={true}
+					value={word}
+					onChange={e => setWord(e.target.value.toUpperCase())}
+					disabled={status !== 'running'}
+				/>
+			</form>
+		</div>
+	)
 }
 
-export default GuessInput;
+export default GuessInput
